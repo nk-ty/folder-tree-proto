ce87abcd115fefa5433cfebabe092a36ccf55b07
naotospace <naotospace@gmail.com>
2022-06-25 09:41:12 +0900

    Change dir
    Fix gem file and add rubocop
    
    Add work dir for setup_ruby
    
    Fix work dir for actions
    
    Disabled style/documetation in rubocop

--- ruby/site_tree.rb
+++ ruby/site_tree.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class SiteTree
   def match(folders, sites)
     all_parent_child = []
-    folders.delete_if{ |folder| folder[:name] == 'ROOT' }
+    folders.delete_if { |folder| folder[:name] == 'ROOT' }
     sites.each do |site|
       parent_child = []
       parent_child << site[:name]
@@ -16,13 +18,13 @@ class SiteTree
 
   def follow_parent(parents, child, parent_child)
     parents.each do |current_parent|
-      if current_parent[:id].to_i == child[:parent_id].to_i
-        parent_child.unshift(current_parent[:name])
-        parents.delete(current_parent)
-        follow_parent(parents, current_parent, parent_child)
-      end
+      next unless current_parent[:id].to_i == child[:parent_id].to_i
+
+      parent_child.unshift(current_parent[:name])
+      parents.delete(current_parent)
+      follow_parent(parents, current_parent, parent_child)
     end
 
-    return parent_child
+    parent_child
   end
 end
